5bceb218be716a24522df95d42ff74d78f23f68e
narantsatsralt-bumnasan <55069134+Narantsatsralt05@users.noreply.github.com>
2022-12-14 10:53:33 +0800

    Title uppercase (#3)
    * Title uppercase
    
    * Changed title of home page
    
    * Used nx affected and admin title changed
    
    * Changed pr yml

--- apps/doclink/pages/_app.tsx
+++ apps/doclink/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to doclink!</title>
+        <title>Welcome to Doclink!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
