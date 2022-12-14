25a94042ceeb9935d0c0ae19d50880a8483adfc9
Narantsatsralt05 <n.bumnasan@gmail.com>
2022-12-14 10:21:55 +0800

    Used nx affected and admin title changed

--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -13,7 +13,7 @@ export function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome admin 👋
+              Welcome Admin 👋
             </h1>
           </div>
 
