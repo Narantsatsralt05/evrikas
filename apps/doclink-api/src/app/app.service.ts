91aa3690604bba417a753130d02539f879d1cffe
Narantsatsralt05 <n.bumnasan@gmail.com>
2022-12-30 15:14:21 +0800

    CI test

--- apps/doclink-api/src/app/app.service.ts
+++ apps/doclink-api/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getData(): { message: string } {
-    return { message: 'Welcome to doclink-api! By CI' };
+    return { message: 'Welcome to doclink-api! By CI Naraa testing for Orgil' };
   }
 }
