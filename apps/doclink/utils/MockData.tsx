faccb936250035e5cb91ad909247efa98ad9d3a2
batorgilm <batorgil.m@nest.edu.mn>
2023-01-09 18:33:00 +0800

    change mockdata

--- apps/doclink/utils/MockData.tsx
+++ apps/doclink/utils/MockData.tsx
@@ -2,89 +2,33 @@ import { useId } from 'react';
 
 export const MockData = [
   {
-    _id: 'id-test-1',
+    _id: 'id-1',
     parentId: 'main',
-    name: 'test-1',
+    name: 'Onboard',
   },
   {
-    _id: 'id-test-2',
-    parentId: 'main',
-    name: 'test-2',
+    _id: 'id-2',
+    parentId: 'id-1',
+    name: 'Developer',
   },
   {
-    _id: 'id-test-3',
-    parentId: 'main',
-    name: 'test-3',
+    _id: 'id-3',
+    parentId: 'id-1',
+    name: 'Back-office',
   },
   {
-    _id: 'id-test-4',
-    parentId: 'id-test-1',
-    name: 'test-4',
+    _id: 'id-4',
+    parentId: 'id-1',
+    name: 'Designer',
   },
   {
     _id: 'id-test-5',
-    parentId: 'id-test-1',
-    name: 'test-5',
+    parentId: 'id-2',
+    name: 'Setup',
   },
   {
     _id: 'id-test-6',
-    parentId: 'id-test-2',
-    name: 'test-6',
-  },
-  {
-    _id: 'id-test-7',
-    parentId: 'id-test-2',
-    name: 'test-7',
-  },
-  {
-    _id: 'id-test-8',
-    parentId: 'id-test-3',
-    name: 'test-8',
-  },
-  {
-    _id: 'id-test-9',
-    parentId: 'id-test-3',
-    name: 'test-9',
-  },
-  {
-    _id: 'id-test-10',
-    parentId: 'id-test-4',
-    name: 'test-10',
-  },
-  {
-    _id: 'id-test-11',
-    parentId: 'id-test-4',
-    name: 'test-11',
-  },
-  {
-    _id: 'id-test-12',
-    parentId: 'id-test-5',
-    name: 'test-12',
-  },
-  {
-    _id: 'id-test-13',
-    parentId: 'id-test-5',
-    name: 'test-13',
-  },
-
-  {
-    _id: 'id-test-14',
-    parentId: 'id-test-6',
-    name: 'test-14',
-  },
-  {
-    _id: 'id-test-15',
-    parentId: 'id-test-6',
-    name: 'test-15',
-  },
-  {
-    _id: 'id-test-16',
-    parentId: 'id-test-7',
-    name: 'test-16',
-  },
-  {
-    _id: 'id-test-17',
-    parentId: 'id-test-7',
-    name: 'test-17',
+    parentId: 'main',
+    name: 'Projects',
   },
 ];
