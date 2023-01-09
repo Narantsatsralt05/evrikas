621fbc4f2cb70909ce725c5f673418bdfb8c06fe
batmunkh0612 <cypox2017@gmail.com>
2023-01-09 18:01:50 +0800

    intialize sidebar

---
+++ apps/doclink/utils/MockData.tsx
@@ -0,0 +1,90 @@
+import { useId } from 'react';
+
+export const MockData = [
+  {
+    _id: 'id-test-1',
+    parentId: 'main',
+    name: 'test-1',
+  },
+  {
+    _id: 'id-test-2',
+    parentId: 'main',
+    name: 'test-2',
+  },
+  {
+    _id: 'id-test-3',
+    parentId: 'main',
+    name: 'test-3',
+  },
+  {
+    _id: 'id-test-4',
+    parentId: 'id-test-1',
+    name: 'test-4',
+  },
+  {
+    _id: 'id-test-5',
+    parentId: 'id-test-1',
+    name: 'test-5',
+  },
+  {
+    _id: 'id-test-6',
+    parentId: 'id-test-2',
+    name: 'test-6',
+  },
+  {
+    _id: 'id-test-7',
+    parentId: 'id-test-2',
+    name: 'test-7',
+  },
+  {
+    _id: 'id-test-8',
+    parentId: 'id-test-3',
+    name: 'test-8',
+  },
+  {
+    _id: 'id-test-9',
+    parentId: 'id-test-3',
+    name: 'test-9',
+  },
+  {
+    _id: 'id-test-10',
+    parentId: 'id-test-4',
+    name: 'test-10',
+  },
+  {
+    _id: 'id-test-11',
+    parentId: 'id-test-4',
+    name: 'test-11',
+  },
+  {
+    _id: 'id-test-12',
+    parentId: 'id-test-5',
+    name: 'test-12',
+  },
+  {
+    _id: 'id-test-13',
+    parentId: 'id-test-5',
+    name: 'test-13',
+  },
+
+  {
+    _id: 'id-test-14',
+    parentId: 'id-test-6',
+    name: 'test-14',
+  },
+  {
+    _id: 'id-test-15',
+    parentId: 'id-test-6',
+    name: 'test-15',
+  },
+  {
+    _id: 'id-test-16',
+    parentId: 'id-test-7',
+    name: 'test-16',
+  },
+  {
+    _id: 'id-test-17',
+    parentId: 'id-test-7',
+    name: 'test-17',
+  },
+];
